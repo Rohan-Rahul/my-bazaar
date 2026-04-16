c266d3fb452143b4b0c748acfe104048c046c778
Rahul <rahulparmarfx@gmail.com>
2026-04-16 22:16:34 +0530

    Cart Drawer Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
+import CartDrawer from './components/CartDrawer';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import ProductDetails from './pages/ProductDetails';
@@ -10,6 +11,7 @@ function App(){
     <CartProvider>
     <Router>
       <Navbar/>
+      <CartDrawer />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
